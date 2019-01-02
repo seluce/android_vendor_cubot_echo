cbc0d019d6d32395745c06d154294e918a20f529
seluce <36071735+seluce@users.noreply.github.com>
2019-01-02 14:18:33 +0100

    First Release

---
+++ Echo-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/Echo/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/Echo/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/Echo/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/Echo/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/Echo/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/Echo/vendor,system/vendor)
---
+++ readme.md
@@ -0,0 +1,11 @@
+Marshmallow vendor blobs from offical Cubot Echo V15
+---------------
+dont replace these files
+
+FM Radio fix
+- lib/libfmjni.so
+
+ fix gallery issues with grey squares
+- lib/libMtkOmxVdecEx.so
+- lib/libMtkOmxVenc.so
+
